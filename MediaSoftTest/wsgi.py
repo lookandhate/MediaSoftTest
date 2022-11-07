f77a5283d91e4e26d53d8cebe0451c985aae2607
lookandhate <princessselestiapwnz@mail.ru>
2022-11-07 20:23:44 +0400

    Добавил начальные данные(пара городов, улиц и магазинов)

--- MediaSoftTest/wsgi.py
+++ MediaSoftTest/wsgi.py
@@ -6,14 +6,46 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
-
+import datetime
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
+from shops.models import City, Street, Shop
 
-admin = User.objects.create_superuser('admin', 'admin@admin.ru', 'admin')
+User = get_user_model()
+
+if not User.objects.filter(email='admin@admin.ru').exists():
+    admin = User.objects.create_superuser('admin', 'admin@admin.ru', 'admin')
+
+    cities = [
+        City(city_name='Ульяновск'),
+        City(city_name='Москва'),
+        City(city_name='Санкт-Петербург'),
+    ]
+
+    City.objects.bulk_create(cities)
+
+    streets = [Street(street_name='Гончарова', city=cities[0]),
+               Street(street_name='Среднеохтинский проспект', city=cities[2]),
+               Street(street_name='Ленина', city=cities[1]),
+               Street(street_name='Брантовская Дорога', city=cities[2]),
+               ]
+    Street.objects.bulk_create(streets)
+
+    shops = [
+        Shop(shop_name='Лента Охта-Молл', street=streets[-1], city=cities[2], home='3А', open_time=datetime.time(0, 0),
+             close_time=datetime.time(23, 59)),
+        Shop(shop_name='Мини Лента', street=streets[-1], city=cities[2], home='3к1', open_time=datetime.time(8, 0),
+             close_time=datetime.time(22, 0)),
+        Shop(shop_name='Гуливер', street=streets[0], city=cities[0], home='1', open_time=datetime.time(8, 0),
+             close_time=datetime.time(22, 0)),
+        Shop(shop_name='1337 Store', street=streets[2], city=cities[1], home='1337', open_time=datetime.time(0, 0),
+             close_time=datetime.time(1, 0)),
+    ]
+
+    Shop.objects.bulk_create(shops)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MediaSoftTest.settings')
 
--- shops/models.py
+++ shops/models.py
@@ -12,12 +12,15 @@ class Street(models.Model):
     street_name = models.CharField(max_length=50)
     city = models.ForeignKey(City, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return f'{self.street_name} in {self.city}'
+
 
 class Shop(models.Model):
     shop_name = models.CharField(max_length=50)
     street = models.ForeignKey(Street, on_delete=models.CASCADE)
     city = models.ForeignKey(City, on_delete=models.CASCADE)
-    home = models.PositiveIntegerField()
+    home = models.CharField(max_length=5)
     open_time = models.TimeField()
     close_time = models.TimeField()
 
