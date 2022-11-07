6a7fde7c9c578d9ffaa2e33fd7bbcac5d293be1e
lookandhate <princessselestiapwnz@mail.ru>
2022-11-07 12:51:37 +0400

    Зарегал модели в админке+заюзал Postgres как локальную БД

--- MediaSoftTest/settings.py
+++ MediaSoftTest/settings.py
@@ -78,8 +78,13 @@ WSGI_APPLICATION = 'MediaSoftTest.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'media_soft_test',
+        'USER': 'media_soft_test',
+        'PASSWORD': '123',
+        'HOST': 'localhost',
+        'PORT': '5432',
+
     }
 }
 
--- shops/admin.py
+++ shops/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from models import *
+from shops.models import *
 
 
 # Register your models here.
